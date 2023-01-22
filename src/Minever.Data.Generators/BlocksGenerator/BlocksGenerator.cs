feef39c4b4dc0dfbdb0af7b4094f83c5d76f0dd8
Ihor Kuzmychov <iikuzmychov@gmail.com>
2023-01-23 01:48:54 +0200

    Crete minimal solution structure

---
+++ src/Minever.Data.Generators/BlocksGenerator/BlocksGenerator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using Minever.Data.Core;
+using Minever.Data.Generators.Utils;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace Minever.Data.Generators;
+
+[Generator]
+public partial class BlocksGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var json   = context.AdditionalFiles.First().GetText(context.CancellationToken)!.ToString();
+        //var json   = @"[ {""id"":0, ""name"": ""air""} ]";
+        var blocks = JsonSerializer.Deserialize<Block[]>(json)!;
+
+        var sourceBuilder = new StringBuilder();
+        sourceBuilder.AppendLine($"using {nameof(Minever)}.{nameof(Data)}.{nameof(Core)};");
+        sourceBuilder.AppendLine();
+        sourceBuilder.AppendLine($"namespace {nameof(Minever)}.{nameof(Data)}.Blocks;");
+        sourceBuilder.AppendLine();
+
+        foreach (var block in blocks)
+        {
+            sourceBuilder.AppendLine(GenerateBlockSourceCode(block));
+            sourceBuilder.AppendLine();
+        }
+
+        context.AddSource("Blocks.g.cs", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+#if DEBUG
+        if (!Debugger.IsAttached)
+            Debugger.Launch();
+#endif
+    }
+
+    private string GenerateBlockSourceCode(Block block) =>
+        $@"public class {block.Name.ToPascalCase()} : {nameof(IBlock)}
+{{
+    public int {nameof(IBlock.Id)} {{ get; }} = {block.Id};
+    public string {nameof(IBlock.Name)} {{ get; }} = {block.Name};
+}}";
+}
---
+++ src/Minever.Data.Generators/Utils/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Minever.Data.Generators.Utils;
+
+internal static class StringExtensions
+{
+    public static string ToPascalCase(this string @string)
+    {
+        var words = @string
+            .Split(new[] { '-', '_' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(word => word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower());
+
+        return string.Concat(words);
+    }
+}
